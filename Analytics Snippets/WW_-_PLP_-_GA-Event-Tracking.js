2d23978e6e38a02e397e0a2335c7560b399a0d77
Dominic Eden <dominic@edenandpartners.com>
2016-03-31 13:39:03 +0100

    Indentation

--- Analytics Snippets/WW_-_PLP_-_GA-Event-Tracking.js
+++ Analytics Snippets/WW_-_PLP_-_GA-Event-Tracking.js
@@ -1,53 +1,63 @@
 $(function(){
-$( '.search-result-content' ).on( 'click', '.thumb-link, .product-action, .name-link, .save_for_later', function(){
-	if ( $( '.sort-by_container > #imagesize' ).val() == 0 )
-	{
-		ga( 'main.set', { 'dimension3': 'large' } );
-	}
-	else
-	{
-		ga( 'main.set', { 'dimension3': 'small' } );
-	}
-	if ( $( '.sort-by_container > #imageview' ).val() == 0 )
-	{
-		ga( 'main.set', { 'dimension4': 'product' } );
-	}
-	else
-	{
-		ga( 'main.set', { 'dimension4': 'model' } );
-	}
-	if ( $( '.sort-by_container > #hideinfo' ).val() == 0 )
-	{
-		ga( 'main.set', { 'dimension5': 'show' } );
-	}
-	else
-	{
-		ga( 'main.set', { 'dimension5': 'hide' } );
-	}
-	ga( 'main.set', { 'dimension6': $( 'form[name="Product-Sorting-Options"] select option:selected' ).text().trim().toLowerCase().replace(/\s/g, '-') } );
-	var purl = $( this ).parents( '.product-tile-grid' ).find( '.name-link' ).attr( 'href' );
-	var pid1 = purl.split( '.html' );
-	var pid2 = pid1[0].split( '/' );
-	var pidf = pid2[ pid2.length - 1 ];
-	var pnam = $( this ).parents( '.product-tile-grid' ).find( '.name-link' ).text().trim().toLowerCase().replace(/\s/g, '-');
-	if ( $( this ).hasClass( 'action-addtocart' ) )
-	{
-		ga('main.ec:addProduct',{
-			'id': 		pidf,
-			'name': 	pnam,
-			'category': digitalData.page.category.id
-		});
-		ga('main.send', 'event', 'plp', 'add-to-bag', pidf + '_-_' + pnam );
-	}
-	else if ( $( this ).hasClass( 'wl-action' ) || $( this ).hasClass( 'save_for_later' ) )
-	{
-		ga('main.send', 'event', 'plp', 'add-to-whishlist', pidf + '_-_' + pnam );
-	}
-	else
-	{
-		ga('main.send', 'event', 'plp', 'view-pdp', pidf + '_-_' + pnam );
-	}
-});
+
+	// When something on the search results page is clicked
+	$( '.search-result-content' ).on( 'click', '.thumb-link, .product-action, .name-link, .save_for_later', function(){
+		if ( $( '.sort-by_container > #imagesize' ).val() == 0 )
+		{
+			ga( 'main.set', { 'dimension3': 'large' } );
+		}
+		else
+		{
+			ga( 'main.set', { 'dimension3': 'small' } );
+		}
+		if ( $( '.sort-by_container > #imageview' ).val() == 0 )
+		{
+			ga( 'main.set', { 'dimension4': 'product' } );
+		}
+		else
+		{
+			ga( 'main.set', { 'dimension4': 'model' } );
+		}
+		if ( $( '.sort-by_container > #hideinfo' ).val() == 0 )
+		{
+			ga( 'main.set', { 'dimension5': 'show' } );
+		}
+		else
+		{
+			ga( 'main.set', { 'dimension5': 'hide' } );
+		}
+
+		ga( 'main.set', { 'dimension6': $( 'form[name="Product-Sorting-Options"] select option:selected' ).text().trim().toLowerCase().replace(/\s/g, '-') } );
+		var purl = $( this ).parents( '.product-tile-grid' ).find( '.name-link' ).attr( 'href' );
+		var pid1 = purl.split( '.html' );
+		var pid2 = pid1[0].split( '/' );
+		var pidf = pid2[ pid2.length - 1 ];
+		var pnam = $( this ).parents( '.product-tile-grid' ).find( '.name-link' ).text().trim().toLowerCase().replace(/\s/g, '-');
+
+		// If the user has clicked to add the item to cart
+		if ( $( this ).hasClass( 'quickviewbutton' ) )
+		{
+			ga('main.ec:addProduct',{
+				'id': 		pidf,
+				'name': 	pnam,
+				'category': digitalData.page.category.id
+			});
+			ga('main.send', 'event', 'Quick buy', 'Click', pidf + '_-_' + pnam );
+		}
+
+		// If the user has clicked to add the item to their wishlist
+		else if ( $( this ).hasClass( 'wl-action' ) || $( this ).hasClass( 'save_for_later' ) )
+		{
+			ga('main.send', 'event', 'plp', 'add-to-whishlist', pidf + '_-_' + pnam );
+		}
+
+		// If the user has clicked the image to view the PDP
+		else
+		{
+			ga('main.send', 'event', 'plp', 'view-pdp', pidf + '_-_' + pnam );
+		}
+	});
+
     // Filters
   $('#main').on('click', '.filters_wrapper li a', function(e) {
     var eventAction = '';
