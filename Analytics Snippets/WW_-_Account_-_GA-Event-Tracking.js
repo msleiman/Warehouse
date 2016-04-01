75a5f577bb7cf374a33931db890aeb2a1e24c350
Dominic Eden <dominic@edenandpartners.com>
2016-04-01 09:44:03 +0100

    WISHLIST: track drag and dropping of items

--- Analytics Snippets/WW_-_Account_-_GA-Event-Tracking.js
+++ Analytics Snippets/WW_-_Account_-_GA-Event-Tracking.js
@@ -9,6 +9,24 @@ $(function(){
     );
   });
 
+  // When a user drags and drops items in their wishlist, fire an event. Wait until the window has loaded for jQuery Sortable to initialise.
+  $(window).load(function(){
+    // Fire event by attaching the change listener to the jQuery UI Sortable plugin.
+    $("#wishlist-items-container").sortable({
+      change: function( event, ui ) {
+        var movedItem = ui.item[0];
+        var movedItemSKU = $(movedItem).find('.saved_item_image > img').attr('src').split('/').pop().split('_')[1];
+        ga(
+          'main.send',
+          'event',
+          'Wishlist',
+          'Drag and Drop',
+          movedItemSKU
+        );
+      }
+    });
+  });
+
   // Submit form to send item to a friend
   $('body').on('click', '#send-to-friend-form .send-button', function() {
     ga(
