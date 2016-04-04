96bee1bde195a7b0926b69831886b000c009c886
Dominic Eden <dominic@edenandpartners.com>
2016-04-04 16:16:20 +0100

    New global Linkshare code that strips the utm parameters from the URL

--- Linkshare/WW_-_Sitewide_-_LinkShare.js
+++ Linkshare/WW_-_Sitewide_-_LinkShare.js
@@ -3,10 +3,19 @@
 // e.g. A referral link might look like http://www.warehouse.co.uk/gb?siteID=1KW2Xiq9xN0-4.c_9w1X8XpO94TU14hg3D
 
 (function(){
-  if (window.location.href.indexOf('?siteID=') > 0) {
-    var linkshareReferrerID = window.location.href.split('?siteID=').pop();
-    document.cookie = 'linkshareReferrerID=' + linkshareReferrerID + ';'; // When does the cookie expire?
-    document.cookie = 'linkshareReferralLastArrivalTime=' + Math.round(new Date().getTime()/1000); + ';'; // The Unix timestamp for when the user first arrived via the referral link.
+  var urlParametersArray = window.location.href.split('&');
+
+  for (var i = 0; i < urlParametersArray.length; i++) {
+    if (urlParametersArray[i].indexOf('_term') > 0) {
+      var linkshareReferrerID = urlParametersArray[i].split('=').pop();
+      console.log(linkshareReferrerID);
+
+      var currentDate = new Date();
+      var currentDatePlus30Days = currentDate.setDate(currentDate.getDate() + 30);
+
+      document.cookie = 'linkshareReferrerID=' + linkshareReferrerID + '; expires=' + new Date(currentDatePlus30Days).toUTCString();
+      document.cookie = 'linkshareReferralLastArrivalTime=' + Math.round(new Date().getTime()/1000); + ';'; // The Unix timestamp for when the user first arrived via the referral link.
+    }
   }
 
 })();
