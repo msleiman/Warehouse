d01427619c8a71333d695b4d8f2713d72082c7fb
Dominic Eden <dominic@edenandpartners.com>
2016-04-06 12:09:49 +0100

    Update digitalData Order Confirmation object with promo codes

--- Linkshare/digitalDataObjectOrderConfirmation.js
+++ Linkshare/digitalDataObjectOrderConfirmation.js
@@ -1,62 +1,71 @@
 var digitalData = {
-   "site":{
-      "country":"GB",
-      "language":"en_GB",
-      "currency":"GBP",
-      "id":"Warehouse-UK"
-   },
-   "page":{
-      "title":"Warehouse - United Kingdom",
-      "id":"GB_OrderConfirmation",
-      "instanceID":"GB_OrderConfirmation_GB_OrderConfirmation",
-      "type":"OrderConfirmation",
-      "url":"https://staging.liam.javelingroup.com/gb/orderconfirmation",
-      "referralUrl":"https://test.adyen.com/hpp/pay.shtml",
-      "random":"196874572147714.97"
-   },
-   "orderId":"04001401",
-   "customer":{
-      "id":"5900003001",
-      "name":{
-         "prefix":"Mr",
-         "firstName":"Dominic",
-         "lastName":"Eden"
+  "site": {
+    "country": "GB",
+    "language": "en_GB",
+    "currency": "GBP",
+    "id": "Warehouse-UK"
+  },
+  "page": {
+    "title": "Warehouse - United Kingdom",
+    "id": "GB_OrderConfirmation",
+    "instanceID": "GB_OrderConfirmation_GB_OrderConfirmation",
+    "type": "OrderConfirmation",
+    "url": "https://www.warehouse.co.uk/gb/orderconfirmation",
+    "referralUrl": "https://live.adyen.com/hpp/pay.shtml",
+    "random": "370536075397471.44"
+  },
+  "orderId": "04009107",
+  "customer": {
+    "id": "5900026503",
+    "name": {
+      "prefix": "",
+      "firstName": "Dominic",
+      "lastName": "Eden"
+    },
+    "email": "dominic@gadabouting.com",
+    "status": "returning",
+    "address": {
+      "houseNumber": "88",
+      "address1": "Prothero Road",
+      "address2": "",
+      "address3": "",
+      "city": "London",
+      "postcode": "SW6 7LZ"
+    }
+  },
+  "bag": {
+    "totals": {
+      "grandTotal": "31.20",
+      "subTotal": "31.20",
+      "tax": "",
+      "discount": "-11.79",
+      "quantity": "1"
+    },
+    "promocodes": "LSTEST20, M3X2X3GKDZBD",
+    "products": [
+      {
+      "id": "0256097010",
+      "price": "39.00",
+      "quantity": "1",
+      "discount": "0.00",
+      "name": "JACQUARD SLIM LEG TROUSER",
+      "colour": "LGRY",
+      "masterCategory": "TROUSERS AND SHORTS"
       },
-      "email":"dom_eden@hotmail.com",
-      "status":"returning",
-      "address":{
-         "houseNumber":"88",
-         "address1":"Prothero Road",
-         "address2":"",
-         "address3":"",
-         "city":"London",
-         "postcode":"SW6 7LZ"
+      {
+        "id": "SAMPLESKU",
+        "price": "31.00",
+        "quantity": "1",
+        "discount": "0.00",
+        "name": "SAMPLE PRODUCT",
+        "colour": "LGRY",
+        "masterCategory": "TROUSERS AND SHORTS"
       }
-   },
-   "bag":{
-      "totals":{
-         "grandTotal":"48.99",
-         "subTotal":"45.00",
-         "tax":"",
-         "discount":"0",
-         "quantity":"1"
-      },
-      "promocodes":"",
-      "products":[
-         {
-            "id":"0257066708",
-            "price":"45.00",
-            "quantity":"1",
-            "discount":"0.00",
-            "name":"Grandad Collar Shirt Dress",
-            "colour":"MWAS",
-            "masterCategory":"DRESSES"
-         }
-      ]
-   },
-   "delivery":{
-      "id":"StandardDelivery",
-      "name":"Standard delivery in 3-5 days",
-      "price":"3.99"
-   }
+    ]
+  },
+  "delivery": {
+    "id": "StandardDelivery",
+    "name": "Standard delivery in 3-5 days",
+    "price": "0.00"
+  }
 }
