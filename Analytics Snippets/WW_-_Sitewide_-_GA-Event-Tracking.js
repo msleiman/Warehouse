add89e0017b211069c9d27bd697bf634f819557a
Dominic Eden <dominic@edenandpartners.com>
2016-04-11 15:14:31 +0100

    SEARCH - fire a virtual pageview when the user searches for a term that we have a redirect (to static page) rule in place for.

--- Analytics Snippets/WW_-_Sitewide_-_GA-Event-Tracking.js
+++ Analytics Snippets/WW_-_Sitewide_-_GA-Event-Tracking.js
@@ -1,5 +1,59 @@
 $(function(){
 
+  // When a user searches, fire a virtual pageview if the search term redirects to a static page instead of a search results page.
+  // Create an object containing the search rules for the site (in the format 'searchTerm': 'redirectURL')
+  var searchRules = {
+    '90mins'                  : '/delivery-and-returns',
+    '90 minute delivery'      : '/delivery-and-returns',
+    '90 minutes'              : '/delivery-and-returns',
+    'delivery'                : '/delivery-and-returns',
+    'Next day'                : '/delivery-and-returns',
+    'Next Day Delivery'       : '/delivery-and-returns',
+    'Nominated Day'           : '/delivery-and-returns',
+    'Nominated Day Delivery'  : '/delivery-and-returns',
+    'returns'                 : '/delivery-and-returns',
+    'Same Day Delivery'       : '/delivery-and-returns',
+    'Same Day Delivery'       : '/delivery-and-returns',
+    'Standard Delivery'       : '/delivery-and-returns',
+    'accessories'             : '/accessories',
+    'accessory'               : '/accessories',
+    'careers'                 : '/join-us',
+    'jobs'                    : '/join-us',
+    'contact'                 : '/contact-us',
+    'contact us'              : '/contact-us',
+    'help'                    : '/contact-us',
+    'denim'                   : '/ware-denim',
+    'jean'                    : '/ware-denim',
+    'jeans'                   : '/ware-denim',
+    'dres'                    : '/clothing/dresses',
+    'dress'                   : '/clothing/dresses',
+    'dresse'                  : '/clothing/dresses',
+    'dresses'                 : '/clothing/dresses',
+    'dresses'                 : '/clothing/dresses',
+    'dresss'                  : '/clothing/dresses',
+    'dressses'                : '/clothing/dresses',
+    'search'                  : '/new-in-all',
+    'warehouse'               : '/new-in-all',
+    'store'                   : '/store-locator',
+    'store info'              : '/store-locator',
+    'store information'       : '/store-locator',
+    'store locator'           : '/store-locator',
+    'store selector'          : '/store-locator',
+    'top'                     : '/all-tops',
+    'tops'                    : '/all-tops'
+  }
+  var searchRulesKeys = Object.keys(searchRules); // Create array of the keys for the searchRules object so we can loop through it.
+
+  $('.header-search form').submit(function(){
+    var searchTerm = $('input#q').val();
+    for (var i = 0; i < searchRulesKeys.length; i++ ) {
+      if (searchTerm == searchRulesKeys[i] ) { // If the search term equals a key in the redirect rules, send a GA virtual pageview.
+        ga('main.send', 'pageview', '/' + digitalData.site.country.toLowerCase() + searchRules[searchRulesKeys[i]] + '/vpv/search/' + searchTerm);
+        ga('rollUp.send', 'pageview', '/' + digitalData.site.country.toLowerCase() + searchRules[searchRulesKeys[i]] + '/vpv/search/' + searchTerm);
+      }
+    }
+  });
+
   // Fire a Virtual Pageview when a user clicks on an auto-suggested search result in the search bar.
   $('body').on('click', '.search-suggestion-right-panel-product', function(){
     var searchTerm = $('input#q').val();
