8a1321ca72da3999444e4040f932d3a7b53f787b
Dominic Eden <dominic@edenandpartners.com>
2016-05-11 11:21:06 +0100

    HOMEPAGE - track half heroes

--- Analytics Snippets/WW_-_Homepage_-_GA-Event-Tracking.js
+++ Analytics Snippets/WW_-_Homepage_-_GA-Event-Tracking.js
@@ -12,12 +12,20 @@ $(function() {
     ga('main.send', 'event', 'Homepage', 'Hero 3', $(this).find('a').attr('href'));
   });
 
-  $('div[data-ga-label="2-slice-left"], div[data-ga-label="2-slice-right"]').click(function(){
-    ga('main.send', 'event', 'Homepage', 'Top Section 1', $(this).find('a').attr('href'));
+  $('div[data-ga-label="2-slice-left"]').click(function(){
+    ga('main.send', 'event', 'Homepage', 'Top Section 1 - 1', $(this).find('a').attr('href'));
   });
 
-  $('div[data-ga-label="9-slice-left"], div[data-ga-label="9-slice-right"]').click(function(){
-    ga('main.send', 'event', 'Homepage', 'Top Section 2', $(this).find('a').attr('href'));
+  $('div[data-ga-label="2-slice-right"]').click(function(){
+    ga('main.send', 'event', 'Homepage', 'Top Section 1 - 2', $(this).find('a').attr('href'));
+  });
+
+  $('div[data-ga-label="9-slice-left"]').click(function(){
+    ga('main.send', 'event', 'Homepage', 'Top Section 2 - 1', $(this).find('a').attr('href'));
+  });
+
+  $('div[data-ga-label="9-slice-right"]').click(function(){
+    ga('main.send', 'event', 'Homepage', 'Top Section 2 - 2', $(this).find('a').attr('href'));
   });
 
 });
