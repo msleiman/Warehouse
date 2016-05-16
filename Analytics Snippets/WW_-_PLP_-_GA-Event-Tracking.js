26d188ad2b50b3e58a1a0783c75eab14595003d0
Dominic Eden <dominic@edenandpartners.com>
2016-05-16 12:01:18 +0100

    PLP - fixes for Quick Buy

--- Analytics Snippets/WW_-_PLP_-_GA-Event-Tracking.js
+++ Analytics Snippets/WW_-_PLP_-_GA-Event-Tracking.js
@@ -39,36 +39,37 @@ $(function(){
 			ga( 'main.set', { 'dimension6': $( 'form[name="Product-Sorting-Options"] select option:selected' ).text().trim().toLowerCase().replace(/\s/g, '-') } );
 			var productImageURL = $( this ).parents( '.product-tile-grid' ).find( '.flex-active-slide .thumb-link > img' ).attr( 'src' );
 			var eightDigitSKU = productImageURL.split('/').pop().split('_')[1];
-			var productName = $( this ).parents( '.product-tile-grid' ).find( '.name-link' ).text().trim().toLowerCase().replace(/\s/g, '-');
+			var sixDigitSKU = eightDigitSKU.substring(0,6);
+			var productName = $(".product-tile[data-itemid='" + sixDigitSKU + "']").find('a.name-link').text().trim();
 
 			// If the user has clicked to add the item to cart (i.e. Quick Buy)
-			if ( $( this ).hasClass( 'quickviewbutton' ) ) {
-
-				setTimeout(function(){
+			if ( $(this).hasClass('quickviewbutton') ) {
+				console.log('quick buy button clicked');
+				setTimeout(function(){ // Set a delay to allow the Quick Buy modal to appear
 					var refreshInterval = setInterval(function(){
-						if ( $('select.variation-select.va-size-select.selectBox option:selected').text().trim() == 'Size') {
-							// Do nothing
-						}
-
-						else {
-							clearInterval(refreshInterval);
-							$('.action-addtocart.product-action.add-to-cart.button_primary').click(function(){
-								ga('main.ec:addProduct',{
-									'id': 		eightDigitSKU,
-									'name': 	productName,
-									'category': digitalData.page.category.id
-								});
-								ga('main.ec:setAction', 'add');
-								ga('main.send', 'event', 'Quick buy', 'Click', eightDigitSKU );
+						if ( $('.product-tile[data-itemid="' + sixDigitSKU + '"]').find('.action-addtocart.product-action.add-to-cart.button_primary').attr('title') == 'Add to Bag') {
+							console.log('found add to bag button');
+							$('.product-tile[data-itemid="' + sixDigitSKU + '"]').find('.action-addtocart.product-action.add-to-cart.button_primary').click(function(){
+							console.log('add to cart button clicked');
+							ga('main.ec:addProduct',{
+								'id': 		eightDigitSKU,
+								'name': 	productName,
+								'category': digitalData.page.category.id
 							});
+							ga('main.ec:setAction', 'add');
+							ga('main.send', 'event', 'Quick buy', 'Click', eightDigitSKU);
+						});
+						clearInterval(refreshInterval);
 						}
-					}, 500)
-
-				}, 1000);
+						else {
+							console.log('Add To Bag not active');
+						}
+					}, 500); // End interval
+				}, 2500); // End setTimeout
 			}
 
 			// If the user has clicked to add the item to their wishlist (aka 'Save Item')
-			else if ( $( this ).hasClass( 'wl-action' ) || $( this ).hasClass( 'save_for_later' ) )	{
+			else if ( $( this ).hasClass( 'save_for_later' ) )	{
 				ga('main.send', 'event', 'Save item', 'Click', eightDigitSKU );
 			}
 
@@ -93,10 +94,7 @@ $(function(){
 					for (var i = 0; i < addressArray.length; i++) {
 						if (addressArray[i].match(/\d+/g) != null) { // If the item in the array contains a number (i.e. is a postcode), send it as a custom dimension.
 							ga( 'main.set', { 'dimension13': addressArray[i].substring(0,3) } );
-							ga( 'rollUp.set', { 'dimension13': addressArray[i].substring(0,3) } );
-
 							ga( 'main.send', 'event', 'Shop by store', 'Store', storeName );
-							ga( 'rollUp.send', 'event', 'Shop by store', 'Store', storeName );
 							break;
 						}
 					}
