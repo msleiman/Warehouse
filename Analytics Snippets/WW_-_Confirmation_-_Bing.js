0da08d6fe6d58afa9aa860ea820ea00b07e01ed2
Dominic Eden <dominic@edenandpartners.com>
2016-06-10 17:00:51 +0100

    Bing tracking

---
+++ Analytics Snippets/WW_-_Confirmation_-_Bing.js
@@ -0,0 +1,6 @@
+// This is for the Bing tracking code that is to be fired on the Order Confirmation page.
+
+(function(){
+  window.uetq = window.uetq || [];
+  window.uetq.push({ 'gv': parseFloat(digitalData.bag.totals.subTotal) });
+}());
