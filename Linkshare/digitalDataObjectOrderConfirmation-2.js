5428b46fc3033adf9f5d61e26acae5ac480a4b02
Dominic Eden <dominic@edenandpartners.com>
2016-07-20 12:41:34 +0100

    Linkshare - second dummy order confirmation object

---
+++ Linkshare/digitalDataObjectOrderConfirmation-2.js
@@ -0,0 +1,71 @@
+var digitalData = {
+  site: {
+    country : "GB",
+    language : "en_GB",
+    currency : "GBP",
+    id : "Warehouse-UK"
+  },
+  page: {
+    title : "Warehouse - United Kingdom",
+    id : "GB_OrderConfirmation",
+    instanceID : "GB_OrderConfirmation_GB_OrderConfirmation",
+    type : "OrderConfirmation",
+    url : window.location.href,
+    referralUrl : document.referrer,
+    random : "977797392167643.6"
+  },
+  orderId: "04157482",
+  customer: {
+    id : "5900026503",
+    name: {
+    prefix : "",
+    firstName : "Dominic",
+    lastName : "Eden"
+    },
+    email : "dominic@gadabouting.com",
+    status : "returning",
+    address: {
+      houseNumber : "Warehouse",
+      address1 : "1 Oliver's Yard",
+      address2 : "",
+      address3 : "",
+      city : "London",
+      postcode : "EC1Y 1SP"
+    }
+  },
+  bag: {
+    totals: {
+      grandTotal : "78.99",
+      subTotal : "75.00",
+      tax : "",
+      discount : "0",
+      quantity : "3"
+    }
+  },
+  promocodes : "",
+  products: [
+    {
+      id : "026923",
+      price : "50.00",
+      quantity : "2",
+      discount : "0.00",
+      name : "PAINT SPOT WOVEN HEM",
+      colour : "White",
+      masterCategory : "TOPS"
+    },
+    {
+    id : "026923",
+    price : "25.00",
+    quantity : "1",
+    discount : "0.00",
+    name : "PAINT SPOT WOVEN HEM",
+    colour : "White",
+    masterCategory : "TOPS"
+    }
+  ],
+  delivery: {
+    id: "StandardDelivery",
+    name: "Standard delivery in 3-5 days",
+    price: "3.99"
+  }
+}
