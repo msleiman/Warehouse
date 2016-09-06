57e5460f1f46aa58ac8a6b9e899871052fb12e48
Dominic Eden <dominic@edenandpartners.com>
2016-09-06 13:06:25 +0100

    Update homepage tracking with promo information

--- Analytics Snippets/WW_-_Homepage_-_GA-Event-Tracking.js
+++ Analytics Snippets/WW_-_Homepage_-_GA-Event-Tracking.js
@@ -2,47 +2,60 @@ $(function() {
 
   // Clicking on a homepage section
   $('#top-hero').click(function(){
-    ga('main.ec:addProduct',{
-      'id': 'homepage_hero_1',
-      'name': 'Homepage Hero 1',
-      'creative': 'wk26_hero_its_a_new_day',
+    ga('main.ec:addPromo',{
+      'id': $(this).find('a').attr('href'),
+      'name': 'Hero',
+      'creative': $('#top-hero').attr('data-ga-creative'),
       'position': '1'
     });
     ga('main.ec:setAction', 'promo_click');
-    ga('main.send', 'event', 'Homepage', 'Hero 1', $(this).find('a').attr('href'));
+    ga('main.send', 'event', 'Homepage', 'Hero', $(this).find('a').attr('href'));
   });
 
   $('.parallax-window a').click(function(){
-    ga('main.ec:addProduct',{
-      'id': 'homepage_hero_2',
-      'name': 'Homepage Hero 2',
-      'creative': 'wk26_parallax',
+    ga('main.ec:addPromo',{
+      'id': $(this).attr('href'),
+      'name': 'Below 1',
+      'creative': $('.parallax-window').attr('data-ga-creative'),
       'position': '2'
     });
     ga('main.ec:setAction', 'promo_click');
-    ga('main.send', 'event', 'Homepage', 'Hero 2', $(this).attr('href'));
+    ga('main.send', 'event', 'Homepage', 'Below 1', $(this).attr('href'));
   });
 
   $('.lookbook-carousel-item a').click(function(){
-    ga('main.ec:addProduct',{
-      'id': 'carousel_bottom',
-      'name': 'Carousel - Shop The Look',
-      'creative': 'wk26_the_lookbook_edit',
+    ga('main.ec:addPromo',{
+      'id': $(this).attr('href'),
+      'name': $(this).attr('data-ga-action'),
+      'creative': $(this).attr('data-ga-creative'),
       'position': '3'
     });
     ga('main.ec:setAction', 'promo_click');
-    ga('main.send', 'event', 'Homepage', 'Shop the look', $(this).attr('href'));
+    ga('main.send', 'event', 'Homepage', $(this).attr('data-ga-action'), $(this).attr('href'));
+  });
+
+  // Video tracking (position 4) is handled in the HTML element itself.
+
+  $('.homepage-split-video-flexslider a').click(function(){
+    ga('main.ec:addPromo',{
+      'id': $(this).attr('href'),
+      'name': 'Below 2',
+      'creative': $(this).attr('data-ga-creative'),
+      'position': '5'
+    });
+    ga('main.ec:setAction', 'promo_click');
+    ga('main.send', 'event', 'Homepage', 'Below 2', $(this).attr('href'));
   });
 
   $('.emmas-edit-carousel-item a').click(function(){
-    ga('main.ec:addProduct',{
-      'id': 'carousel_bottom',
-      'name': 'Carousel - Bottom',
-      'creative': 'wk26_emmas_edit_carousel',
+    ga('main.ec:addPromo',{
+      'id': $(this).attr('href'),
+      'name': $(this).attr('data-ga-action'),
+      'creative': $(this).attr('data-ga-creative'),
       'position': '6'
     });
     ga('main.ec:setAction', 'promo_click');
-    ga('main.send', 'event', 'Homepage', 'Carousel - Bottom', $(this).attr('href'));
+    ga('main.send', 'event', 'Homepage', $(this).attr('data-ga-action'), $(this).attr('href'));
   });
 
 
